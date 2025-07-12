15d8a3b8487107e2d223bfd2896749eaa9cac91c
Javid Ahmed <javidahmed@icloud.com>
2025-07-12 02:45:00 +0100

    Fix logging date format to use 24-hour time representation

--- src/mouse_controls/main.py
+++ src/mouse_controls/main.py
@@ -7,7 +7,7 @@ from pynput.keyboard import Key
 from mouse_controls.models.mouse import Mouse
 from mouse_controls.models.mouse_listener import MouseListener
 
-logging.basicConfig(format="%(asctime)s %(message)s", datefmt="[%d-%m-%Y|%I:%M:%S]", level=logging.DEBUG)
+logging.basicConfig(format="%(asctime)s %(message)s", datefmt="[%d-%m-%Y|%H:%M:%S]", level=logging.DEBUG)
 
 POS_LIMS = ((0, 1920), (0, 1080))
 POS_BUFFER = 10
