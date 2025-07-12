c6756800edfe2c6bb67d7f38a80c955688ad8d44
Javid Ahmed <javidahmed@icloud.com>
2025-07-12 02:44:04 +0100

    Refactor mouse initialization parameters for improved readability

--- src/mouse_controls/main.py
+++ src/mouse_controls/main.py
@@ -9,6 +9,8 @@ from mouse_controls.models.mouse_listener import MouseListener
 
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt="[%d-%m-%Y|%I:%M:%S]", level=logging.DEBUG)
 
+POS_LIMS = ((0, 1920), (0, 1080))
+POS_BUFFER = 10
 DELTA_TIME = 0.001
 LOCK_TOGGLE_BUTTON = Key.ctrl_r
 EXIT_BUTTON = Key.end
@@ -17,8 +19,8 @@ EXIT_BUTTON = Key.end
 def main() -> None:
     """Start the mouse control thread."""
     mouse = Mouse(
-        pos_lims=((0, 1920), (0, 1080)),
-        pos_buffer=10,
+        pos_lims=POS_LIMS,
+        pos_buffer=POS_BUFFER,
         delay=DELTA_TIME,
         lock_toggle_btn=LOCK_TOGGLE_BUTTON,
         exit_btn=EXIT_BUTTON,
